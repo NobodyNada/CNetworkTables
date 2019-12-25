e61f0cb3e90bb1223416cf675c46fb8dd2d24502
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2019-12-25 11:36:58 -0800

    make C++ headers private

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CNetworkTables",
-            dependencies: ["CWPIUtil"]),
+            dependencies: ["CWPIUtil"],
+            cxxSettings: [.headerSearchPath("cpp/include")]),
         .target(
             name: "CWPIUtil",
             dependencies: []),
